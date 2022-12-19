3b15cc66c13cd70fb4bf1120818ec2b5d0e0922f
skyeflier <119133572+skyeflier@users.noreply.github.com>
2022-12-18 21:14:31 -0600

    added pseudo code

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment code here
 
+// Prompt element that provides password criteria
+let intro = prompt("Generate a random password!");
+console.log(intro)
+
+// Criteria - add password special characters
+
+// Length - set min length to 8 and max to 128
+
+// Character Types - determine which to include/exclude
+
+// If / Else If Statement - Checking off each character type criteria
+
+// Else - Make final statement to ensure all criteria is met
+
+// Alert - Create alert to be displayed
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
