0e3e804acbb4f1d0cc13550324b6bca72bfe6a7d
skyeflier <119133572+skyeflier@users.noreply.github.com>
2022-12-22 21:05:10 -0600

    final commit

--- script.js
+++ script.js
@@ -4,33 +4,28 @@ const characters = ["!", "#", "$"];
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Create a function that prompts users for input, validates them and creates the password.
-function generatePassword() {
+function generatePassword() { // Create a function that prompts users for input, validates them and creates the password.
 
-    // Create an empty array for the final password to go into
-    let passwordFinal = [];
+    let passwordFinal = []; // Create an empty array for the final password to go into
 
     let selectedCharacters = [];
 
-    //Create a function that prompts questions to user. Use console.log to check if code is running
-    let numberLengthResponse = prompt("Hey friend! Please select a password length between 8-128.");
+    let numberLengthResponse = prompt("Hey friend! Please select a password length between 8-128."); //Create a function that prompts questions to user. Use console.log to check if code is running
     parseInt(numberLengthResponse < 8 || numberLengthResponse > 128) //Make it so users have to include a number
-    let numbersConfirm = confirm("If you'd like a number, click OK.")
-    let characterConfirm = confirm("If you'd like some cool characters, click OK.");
-    let lowerCaseConfirm = confirm("If you'd like some lavish lowercase letters, click OK.");
-    let upperCaseConfirm = confirm("If you'd like some ugly uppercase letters, click OK.");
+    let numbersConfirm = confirm("If you'd like some nutty numbers, click OK.")
+    let characterConfirm = confirm("If you'd like some cattywampus characters, click OK.");
+    let lowerCaseConfirm = confirm("If you'd like some lackluster lowercase letters, click OK.");
+    let upperCaseConfirm = confirm("If you'd like some unloved uppercase letters, click OK.");
 
-    // Create a loop that checks if the use didn't select ANY values, they need to start over
-    if (numbersConfirm === false &&
+    if (numbersConfirm === false && // Create a loop that checks if the use didn't select ANY values, they need to start over
         characterConfirm === false &&
         lowerCaseConfirm === false &&
         upperCaseConfirm === false) {
         alert("Hey you silly goose, you gotta pick at least one value to create a password — try again!");
     }
 
-    //Use JS Array push() loop to push the users confirmed value selections to the passwordFinal value. 
-    if (numberLengthResponse < 8 || numberLengthResponse > 128) {
-        alert("Hey make sure you have 8-128 characters.")
+    if (numberLengthResponse < 8 || numberLengthResponse > 128) {  //Use JS Array push() loop to push the users confirmed value selections to the passwordFinal value. 
+        alert("Oopsie daisy! You gotta make sure you have 8-128 characters, my friend!")
     };
     if (numbersConfirm) {
         selectedCharacters.push(...numbers)
@@ -46,7 +41,7 @@ function generatePassword() {
     };
     console.log(selectedCharacters)
 
-    for (let i = 0; i < numberLengthResponse; i++) {
+    for (let i = 0; i < numberLengthResponse; i++) {     //Create a for loop that will cycle through the function above to select each character at random to the length designated by the user. 
         let randomNum = Math.floor(Math.random() * selectedCharacters.length);
         let randomChar = selectedCharacters[randomNum];
         passwordFinal.push(randomChar);
@@ -57,13 +52,11 @@ function generatePassword() {
 
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input - needs to be a certain length, random, and a loop
-function writePassword() {
+function writePassword() { // Write password to the #password input - needs to be a certain length, random, and a loop
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // Add event listener to generate button
